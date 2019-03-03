5351b4b3f92a8dec7e9fa08ae25daa7482e71e2a
Unknown <1643216@cgmatane.qc.ca>
2019-03-02 19:09:30 -0500

    Ajout du champs source au client #4

--- Clients/ClientVanille/src/modele/DecodeurPenseesXML.java
+++ Clients/ClientVanille/src/modele/DecodeurPenseesXML.java
@@ -38,15 +38,18 @@ public class DecodeurPenseesXML {
                 String auteur = noeudPensee.getElementsByTagName("auteur").item(0).getTextContent();
                 String message = noeudPensee.getElementsByTagName("message").item(0).getTextContent();
                 String annee = noeudPensee.getElementsByTagName("annee").item(0).getTextContent();
+                String source = noeudPensee.getElementsByTagName("source").item(0).getTextContent();
 
                 Journal.ecrire(3, "Id : " + id);
                 Journal.ecrire(3, "Auteur : " + auteur);
                 Journal.ecrire(3, "Message : " + message);
                 Journal.ecrire(3, "Annee : " + annee);
+                Journal.ecrire(3, "Source : " + source);
 
                 Pensee pensee = new Pensee(auteur, message);
                 //pensee.setAnnee(Integer.parseInt(annee));
                 pensee.setId(Integer.parseInt(id));
+                pensee.setSource(source);
                 listePensees.add(pensee);
             }
         } catch (ParserConfigurationException e) {
@@ -75,15 +78,18 @@ public class DecodeurPenseesXML {
             String auteur = noeudPensee.getElementsByTagName("auteur").item(0).getTextContent();
             String message = noeudPensee.getElementsByTagName("message").item(0).getTextContent();
             String annee = noeudPensee.getElementsByTagName("annee").item(0).getTextContent();
+            String source = noeudPensee.getElementsByTagName("source").item(0).getTextContent();
 
             Journal.ecrire(3, "Id : " + id);
             Journal.ecrire(3, "Auteur : " + auteur);
             Journal.ecrire(3, "Message : " + message);
             Journal.ecrire(3, "Annee : " + annee);
+            Journal.ecrire(3, "Source : " + source);
 
             Pensee pensee = new Pensee(auteur, message);
             //pensee.setAnnee(Integer.parseInt(annee));
             pensee.setId(Integer.parseInt(id));
+            pensee.setSource(source);
 
             return pensee;
         } catch (ParserConfigurationException e) {
--- Clients/ClientVanille/src/modele/Pensee.java
+++ Clients/ClientVanille/src/modele/Pensee.java
@@ -6,7 +6,8 @@ public class Pensee {
 	protected String auteur;
 	protected String message;
 	protected int annee;
-	
+	protected String source;
+
 	
 	public Pensee(String auteur, String message) {
 		super();
@@ -38,5 +39,10 @@ public class Pensee {
 	public void setAnnee(int annee) {
 		this.annee = annee;
 	}
-
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
 }
